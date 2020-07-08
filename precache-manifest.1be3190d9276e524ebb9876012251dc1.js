61640ba25bb3104bd5045fc3a4297aa0cc33b23d
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-07-09 01:09:56 +1200

    Updates

---
+++ precache-manifest.1be3190d9276e524ebb9876012251dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a8bf13298d309681662c884bee4562",
+    "url": "/React-GraphQL/index.html"
+  },
+  {
+    "revision": "d01374830de2ece0c1a1",
+    "url": "/React-GraphQL/static/css/main.19b86f77.chunk.css"
+  },
+  {
+    "revision": "b454ec3edd30adc9bdc5",
+    "url": "/React-GraphQL/static/js/2.d7911e47.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/React-GraphQL/static/js/2.d7911e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01374830de2ece0c1a1",
+    "url": "/React-GraphQL/static/js/main.e3557315.chunk.js"
+  },
+  {
+    "revision": "21de126bd14dda73b75a",
+    "url": "/React-GraphQL/static/js/runtime-main.27f87f7f.js"
+  }
+]);
